cf292ad77aeec585c358cedb1e8cab378d2bd857
Kjell Fjeldsaunet <kjell.fjeldsaunet@gmail.com>
2020-07-06 22:03:59 +0200

    Basic drawing functionality implemented

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-const gridContainer = document.querySelector('.grid-container');//Create drawing area
+const gridContainer = document.querySelector('.grid-container');//get drawing area
 
 
 //create the grid of pixel-divs
 function createDrawingArea(gridSize) {
-    numberOfPixels = Math.pow(gridSize, 2);
+    const grid = document.createElement('div');//create grid for pixels
+    grid.classList.add('grid')
+    grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
+    grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
+    gridContainer.appendChild(grid);
+
+    numberOfPixels = gridSize * gridSize;
     for (let i = 0; i < numberOfPixels; i++) {
         const gridSquare = document.createElement('div');//create pixel div
         gridSquare.classList.add('grid-square')//assign "pixel"-class to pixel div
-        gridContainer.appendChild(gridSquare);
-        console.log(i)
+        gridSquare.addEventListener('mouseover', (e) => {
+            e.target.style.backgroundColor = '#fff'
+        })
+        grid.appendChild(gridSquare);
     }
 }
